e7e3c7cafe7166e648526ee887bf4ba2ef774b09
Simon He <57086651+Simon-He95@users.noreply.github.com>
2024-03-21 11:51:25 +0800

    chore: 添加转换逻辑

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,45 @@
-import { getLocale, message } from '@vscode-use/utils'
+import { addEventListener, createRange, getConfiguration, getPosition, updateText } from '@vscode-use/utils'
 import type { Disposable, ExtensionContext } from 'vscode'
 
 export async function activate(context: ExtensionContext) {
   const disposes: Disposable[] = []
-  const lan = getLocale()
-  const isZh = lan.includes('zh')
-  message.info(isZh ? '你好' : 'Hello')
+  let mappings = getConfiguration('symbol-mapping-conversion.mappings')
+  const isRun = true
+  disposes.push(addEventListener('text-change', (e) => {
+    if (!isRun)
+      return
+    const changes = e.contentChanges.filter((c: any) => c.text.trim())
+    if (!changes.length)
+      return
+    const updateLists: any = []
+    changes.forEach((c: any) => {
+      let text = c.text
+      Object.keys(mappings).forEach((k) => {
+        const v = mappings[k]
+        text = text.replaceAll(k, v)
+      })
+      if (text !== c.text) {
+        const start = getPosition(c.rangeOffset)
+        const end = getPosition(c.rangeOffset + c.text.length)
+        const range = createRange(start, end)
+        updateLists.push({
+          range,
+          text,
+        })
+      }
+    })
+    if (!updateLists.length)
+      return
+    updateText((edit) => {
+      updateLists.forEach((list: any) => {
+        edit.replace(list.range, list.text)
+      })
+    })
+  }))
+
+  disposes.push(addEventListener('config-change', () => {
+    mappings = getConfiguration('symbol-mapping-conversion.mappings')
+  }))
 
   context.subscriptions.push(...disposes)
 }
